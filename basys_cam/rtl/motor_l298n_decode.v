071c4da717d525edb48d09a4f7df8740770da99c
Vmagic0 <sg.twardosz@gmail.com>
2026-06-03 14:07:28 +0200

    fixed robot control

--- basys_cam/rtl/motor_l298n_decode.v
+++ basys_cam/rtl/motor_l298n_decode.v
@@ -1,19 +1,23 @@
 // Pad / SPI MISO[3:0] -> L298N patterns.
-// motor_in[0]=IN1, [1]=IN2 (motor 1); [2]=IN3, [3]=IN4 (motor 2)
+// motor_in[0]=IN1, [1]=IN2 (right motor); [2]=IN3, [3]=IN4 (left motor)
 // ctrl_nibble: bit0=up, bit1=right, bit2=down, bit3=left
 //
-// In this robot one motor is mounted/wired with opposite polarity, so straight
-// forward is IN1=0 IN2=1 | IN3=1 IN4=0.
+// Wiring:
+//   right motor: red=OUT1, black=OUT2
+//   left motor:  black=OUT3, red=OUT4
+// Because the left motor is wired opposite to the right one, straight forward is:
+//   right: IN1=1 IN2=0
+//   left:  IN3=0 IN4=1
 module motor_l298n_decode (
     input  wire [3:0] ctrl_nibble,
     output reg  [3:0] motor_in
 );
 
     localparam [3:0] MOT_STOP  = 4'b0000;
-    localparam [3:0] MOT_FWD   = 4'b0110;
-    localparam [3:0] MOT_REV   = 4'b1001;
-    localparam [3:0] MOT_LEFT  = 4'b0101;
-    localparam [3:0] MOT_RIGHT = 4'b1010;
+    localparam [3:0] MOT_FWD   = 4'b1001; // IN1=1 IN2=0, IN3=0 IN4=1
+    localparam [3:0] MOT_REV   = 4'b0110; // IN1=0 IN2=1, IN3=1 IN4=0
+    localparam [3:0] MOT_LEFT  = 4'b1000; // right motor stop, left motor forward
+    localparam [3:0] MOT_RIGHT = 4'b0001; // right motor forward, left motor stop
 
     wire up    = ctrl_nibble[0];
     wire right = ctrl_nibble[1];
@@ -23,14 +27,8 @@ module motor_l298n_decode (
     always @(*) begin
         if ((up && down) || (left && right))
             motor_in = MOT_STOP;
-        else if (up && !down) begin
-            if (left && !right)
-                motor_in = MOT_LEFT;
-            else if (right && !left)
-                motor_in = MOT_RIGHT;
-            else
-                motor_in = MOT_FWD;
-        end
+        else if (up && !down)
+            motor_in = MOT_FWD;
         else if (down && !up)
             motor_in = MOT_REV;
         else if (left && !right)
